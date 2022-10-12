6202523e9fb52a48d5eae983cca9dec0b995287f
Gayugai3 <112932746+Gayugai3@users.noreply.github.com>
2022-10-12 12:22:21 +0530

    Onclick

---
+++ script.js
@@ -0,0 +1,9 @@
+function check()
+{
+    var fn=document.getElementById("fname").value;
+    var mn=document.getElementById("mname").value;
+    var ln=document.getElementById("lname").value;
+    var mail=document.getElementById("email").value;
+    var pswrd=document.getElementById("pass").value;
+     console.log(fn,mn,ln,mail,pswrd);
+}
